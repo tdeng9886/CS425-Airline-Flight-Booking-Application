629a8aaedaf385dcc23d5f08873aa8d56bcb3686
Dustin Van Tate Testa <toast27@gmail.com>
2019-11-04 19:32:38 -0600

    checkAuth fxn, called automatically when we link this script

---
+++ frontend/main.js
@@ -0,0 +1,23 @@
+
+// 
+const util = {
+    getCookie: cname => {
+        const name = cname + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ')
+                c = c.substring(1);
+            if (c.indexOf(name) == 0)
+                return c.substring(name.length, c.length);
+        }
+        return "";
+    },
+};
+
+// automatically check authentication, if it's invalid, redirect to login page
+(function checkAuth() {    
+    if (!util.getCookie('authToken'))
+        window.location = 'signin.html';
+})();
