cb362bffce5355910acbaefe054f34f87fe9eaab
Justin Schmitz <jschmitz2@hawk.iit.edu>
2019-11-10 15:54:26 -0600

    Update flights_routes.py

--- app/routes/flights_routes.py
+++ app/routes/flights_routes.py
@@ -2,6 +2,7 @@ from app import app
 from app.flightBookerDB import db_interface
 from flask import request
 
+
 @app.route('/flights/airports', methods=['POST'])
 def getAirports():
     return db_interface.getAirports()
@@ -9,17 +10,33 @@ def getAirports():
 # Taking two airportIds:
 @app.route('/flights/search', methods=['POST'])
 def routeFlight():
-    data = request.json
+    def routeFlight_rec(cur_airport, arriveTime, tokens=5):
+        if tokens == 0:
+            return False
 
+        routes = []
+        completed_routes = []
+        for row in db_interface.getFlights(cur_airport, arriveTime):
+            routes.append([row])
+
+        subroutes = {}
+        for route in routes:  # If the end point is where we want to go:
+            if route[3] == end:  # If this route ends at the correct place:
+                completed_routes.append(route)
+            else:  # Otherwise, find it
+                subroute = routeFlight_rec(route[3], route[7], tokens-1))
+                if subroute:
+                    subroutes[route] = subroute
+        for key in subroutes.keys():
+            for subroute in subroutes[key]:
+                completed_routes.append(key.extend(subroute))
+
+        return completed_routes
+
+    data = request.json
     departAirportId = data['departAirportId']
     departTime = data['departTime']
     end = data['end']
-    visited_flights = set()
 
-    def getFlights(departAirportId, departTime):
-        return list(db_interface.getFlights(departAirportId, departTime))
-
-    routes = []
-    while len(routes) < 10:
-        # Begin BFSing until we find a path.
-        return None
+    routes = routeFlight_rec(departAirportId, departTime)
+    return route
