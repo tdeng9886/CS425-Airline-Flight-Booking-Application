7e9291a40894d7ad4e8014b36756e2ce9626b73b
dvtate <toast27@gmail.com>
2019-11-19 13:58:52 -0600

    minor tweaks

--- app/routes/static_routes.py
+++ app/routes/static_routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import Flask, request, send_from_directory,redirect
 
+
 @app.route('/web/<path:path>')
 def static_route(path):
     return send_from_directory('../frontend', path)
